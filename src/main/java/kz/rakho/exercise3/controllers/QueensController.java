253efe357218bde5e6ad7377c29a97f48af61651
RakhatS <rakhat.saparbaev@gmail.com>
2024-01-29 10:36:20 +0600

    ____

--- src/main/java/kz/rakho/exercise3/controllers/QueensController.java
+++ src/main/java/kz/rakho/exercise3/controllers/QueensController.java
@@ -2,6 +2,8 @@ package kz.rakho.exercise3.controllers;
 
 
 import java.util.ArrayList;
+import java.util.Random;
+
 import kz.rakho.exercise3.models.QueenPosition;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,48 +18,25 @@ class QueensController {
     public QueenPosition[] placeQueens(@RequestBody QueenPosition queenPosition) {
         int n = 8;
         QueenPosition[] queens = new QueenPosition[8];
-        ArrayList<int[]> queensPositions = solveNQueensGivenFirstQueen(queenPosition.getRow(), queenPosition.getCol(), n);
-
-        for (int i = 0; i < queensPositions.get(0).length; i++) {
-            QueenPosition qp = new QueenPosition(i + 1, (queensPositions.get(0)[i] + 1));
-            queens[i] = qp;
-
-        }
+        queens[0] = queenPosition;
+        setOtherQueensPosition(queens);
         return queens;
     }
 
-    private boolean isSafe(int[] board, int row, int col, int n) {
-        for (int i = 0; i < row; i++) {
-            if (board[i] == col || board[i] - i == col - row || board[i] + i == col + row) {
-                return false;
+    private void setOtherQueensPosition(QueenPosition[] queens){
+        Random random = new Random();
+        for (int i = 1; i < queens.length;){
+            queens[i] = new QueenPosition(random.nextInt(8), random.nextInt(8));
+            boolean isDupExist = false;
+            for (int j = 0; j < i; j ++){
+                if (queens[j].getCol() == queens[i].getCol() && queens[j].getRow() == queens[i].getRow() ){
+                    isDupExist = true;
+                    break;
+                }
             }
-        }
-        return true;
-    }
-
-    private void solveNQueensUtil(int[] board, int row, int n, ArrayList<int[]> result) {
-        if (row == n) {
-            result.add(board.clone());
-            return;
-        }
-
-        for (int col = 0; col < n; col++) {
-            if (isSafe(board, row, col, n)) {
-                board[row] = col;
-                solveNQueensUtil(board, row + 1, n, result);
-            }
+            if (!isDupExist)
+                i++;
         }
     }
-
-    private ArrayList<int[]> solveNQueensGivenFirstQueen(int firstQueenRow, int firstQueenCol, int n) {
-        int[] board = new int[n];
-        board[firstQueenRow] = firstQueenCol;
-
-        ArrayList<int[]> result = new ArrayList<>();
-        solveNQueensUtil(board, firstQueenRow + 1, n, result);
-
-        return result;
-    }
-
 
 }
